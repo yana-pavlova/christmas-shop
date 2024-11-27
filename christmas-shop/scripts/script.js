288dbc45ca6798ae468b836454ab724a207660b9
Yana Pavlova <yn-pvlv@yandex.ru>
2024-11-27 17:56:49 +0300

    feat: implement timer functionality

--- christmas-shop/scripts/script.js
+++ christmas-shop/scripts/script.js
@@ -81,7 +81,40 @@ sliderLeftBtn.onclick = () => {
   }
 }
 
+// TIMER
+const days = document.querySelector('.cta-timer-days');
+const hours = document.querySelector('.cta-timer-hours');
+const minutes = document.querySelector('.cta-timer-minutes');
+const seconds = document.querySelector('.cta-timer-seconds');
 
+const newYearDate = new Date('2025-01-01');
+
+let d, diff, data;
+
+const calculateTimeDifference = (seconds) => {
+  seconds = Math.trunc(seconds);
+  let minutes = Math.trunc(seconds / 60);
+  seconds = seconds - (minutes * 60);
+  let hours = Math.trunc(minutes / 60);
+  minutes = minutes - (hours * 60);
+  let days = Math.trunc(hours / 24);
+  hours = hours - (days * 24);
+
+  return {days, hours, minutes, seconds};
+}
+
+setInterval(() => {
+  d = new Date();
+  diff = (d.getTime() - newYearDate.getTime()) / 1000;
+  data = calculateTimeDifference(diff);
+  
+  days.textContent = data.days * -1;
+  hours.textContent = data.hours * -1;
+  minutes.textContent = data.minutes * -1;
+  seconds.textContent = data.seconds * -1;
+}, 1000);
+
+// WINDOW LISTENERS
 window.addEventListener('resize', () => {
   if(window.innerWidth > 768 && icon.classList.contains('close-icon')) {
     icon.classList.toggle('close-icon');
